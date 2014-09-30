bbae02b9c7262eef57c60f5a331394bbd6ab1ed8
Lee Smith <LeePaulSmith@googlemail.com>
2014-09-30 16:07:15 +0100

    Azure Storage

---
+++ Instanda.Sequence.Azure/AzureTableStorageProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using System.Threading.Tasks;
+
+namespace Instanda.Sequence.Azure
+{
+    public class AzureTableStorageProvider : TableEntityRepository<SequenceTableEntity>, IStateProvider
+    {
+        private const string PartitionKey = "Sequences";
+
+        public AzureTableStorageProvider(string connectionStr,string tableName):base(connectionStr,tableName,false)
+        {
+            
+        }
+
+        public async Task<SequenceKey> AddAsync(Sequence sequence)
+        {
+            var rowKey = Guid.NewGuid().ToString();
+
+            var sequenceEntity = SequenceTableEntity.FromSequence(sequence);
+
+            sequenceEntity.RowKey = rowKey;
+            sequenceEntity.PartitionKey = PartitionKey;
+
+            await base.Add(sequenceEntity);
+
+            return new SequenceKey { Value = rowKey };
+        }
+
+        public async Task<Sequence> GetAsync(SequenceKey sequenceKey)
+        {
+            var sequenceEntity = await base.Get(PartitionKey, sequenceKey.Value);
+
+            return sequenceEntity.ToSequence();
+        }
+
+        public async  Task<bool> UpdateAsync(SequenceKey sequenceKey, Sequence sequence)
+        {
+            var sequenceEntity = SequenceTableEntity.FromSequence(sequence);
+
+            sequenceEntity.RowKey = sequenceKey.Value;
+            sequenceEntity.PartitionKey = PartitionKey;
+
+           var updatedSequenceEntity  = await UpdateAsync(sequenceEntity);
+
+           return updatedSequenceEntity != null;
+        }
+    }
+}
---
+++ Instanda.Sequence.Azure/SequenceTableEntity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Instanda.Sequence.Azure
+{
+    public class SequenceTableEntity:TableEntity
+    {
+
+      
+
+        public long StartAt { get; set; }
+        public int Increment { get; set; }
+        public long MaxValue { get; set; }
+        public long MinValue { get; set; }
+        public bool Cycle { get; set; }
+
+        public long CurrentValue { get; set; }
+
+
+        public  Sequence ToSequence()
+        {
+            return new Sequence { 
+             CurrentValue = CurrentValue,
+              Cycle = Cycle,
+               Increment = Increment,
+                MaxValue = MaxValue,
+                 MinValue = MinValue,
+                  StartAt = StartAt
+            };
+        }
+
+        public static SequenceTableEntity FromSequence( Sequence sequence)
+        {
+
+            return new SequenceTableEntity
+            {
+                CurrentValue = sequence.CurrentValue,
+                Cycle = sequence.Cycle,
+                Increment = sequence.Increment,
+                MaxValue = sequence.MaxValue,
+                MinValue = sequence.MinValue,
+                StartAt = sequence.StartAt
+            };
+        }
+    }
+
+    
+
+}
