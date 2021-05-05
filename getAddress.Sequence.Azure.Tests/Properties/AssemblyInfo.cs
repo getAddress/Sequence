067daf3d4a1d4bc7b94364905676086fe0ae3246
Lee Smith <LeePaulSmith@googlemail.com>
2021-05-05 13:34:03 +0100

    Update AssemblyInfo.cs

--- getAddress.Sequence.Azure.Tests/Properties/AssemblyInfo.cs
+++ getAddress.Sequence.Azure.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Instanda.Sequence.Azure.Tests")]
+[assembly: AssemblyTitle("GetAddress.Sequence.Azure.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Instanda.Sequence.Azure.Tests")]
+[assembly: AssemblyProduct("GetAddress.Sequence.Azure.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
